7a8e4d5a1423afa17930b7d2e466832d60cb79f0
Augustin Laurent <augustin.lrt2@icloud.com>
2023-06-22 17:47:03 +0200

    Modification de la fonction recover pour fix l'erreur JSON de parsing

--- cogs/TFT.py
+++ cogs/TFT.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import requests
 import asyncio
 import json
+import os
 from helpers import checks, create_decoders as decoder, helpers, talkies
 import keys
 
@@ -208,46 +209,42 @@ class TFT(commands.Cog):
     @commands.check(checks.check_if_bot)
     async def register(self, ctx, region_code, summoner_name):
         """
-        Enregiste un nouveau joueur dans la base de données de tracking du bot.
+        Enregistre un nouveau joueur dans la base de données de suivi du bot.
         """
         try:
             region_route = decoder.region[region_code.upper()]
         except Exception as e:
-            print("Erreur lors récupération du code région: ", e)
+            print("Erreur lors de la récupération du code région: ", e)
         try:
             APIlink = f"https://{region_route}.api.riotgames.com/tft/summoner/v1/summoners/by-name/{summoner_name}"
             summoner_data = requests.get(APIlink, headers=self.headers)
-            summoner_id = summoner_data.json().get("id")
-            summoner_name = summoner_data.json().get("name")
-            try:
-                with open('summoners.json', 'r') as f:
-                    summoners = json.load(f)
-            except FileNotFoundError:
-                print("Le fichier contenant les joueurs n'a pas pu être chargé, tentative de création de celui-ci.")
+            if summoner_data.status_code == 200:
                 try:
-                    with open('summoners.json', 'x') as f:
-                        summoners = json.load(f)
-                except FileNotFoundError:
-                    print("Le fichier n'a pas pu être créé, vérifiez que vous avez les droits d'écriture dans le répertoire source.")
-            if summoner_id not in summoners:
-                summoners[summoner_id] = summoner_name
-                try:
-                    with open('summoners.json', 'w') as f:
-                        json.dump(summoners, f)
-                    embed_msg = discord.Embed()
-                    embed_msg.add_field(name="Message", value=f"Le joueur {summoner_name} a bien été enregistré.", inline=False),
-                    embed_msg.colour=discord.Colour.blue()
-                    await ctx.send(embed=embed_msg)
-                except FileNotFoundError:
-                    embed_msg = discord.Embed()
-                    embed_msg.add_field(name="Message", value=f"Le joueur {summoner_name} n'a pas pu être enregistré, contacter l'administrateur.", inline=False),
-                    embed_msg.colour=discord.Colour.red()
-                    await ctx.send(embed=embed_msg)
+                    summoner_data_json = summoner_data.json()
+                    summoner_id = summoner_data_json.get("id")
+                    summoner_name = summoner_data_json.get("name")
+                    if os.path.exists("summoners.json") and os.path.getsize("summoners.json") > 0:
+                        with open('../utility/summoners.json', 'r') as f:
+                            summoners = json.load(f)
+                    else:
+                        summoners = {}
+                    if summoner_id not in summoners:
+                        summoners[summoner_id] = summoner_name
+                        with open('../utility/summoners.json', 'w') as f:
+                            json.dump(summoners, f)
+                        embed_msg = discord.Embed()
+                        embed_msg.add_field(name="Message", value=f"Le joueur {summoner_name} a bien été enregistré.", inline=False),
+                        embed_msg.colour = discord.Colour.blue()
+                        await ctx.send(embed=embed_msg)
+                    else:
+                        embed_msg = discord.Embed()
+                        embed_msg.add_field(name="Message", value=f"Le joueur {summoner_name} est déjà enregistré.", inline=False),
+                        embed_msg.colour = discord.Colour.green()
+                        await ctx.send(embed=embed_msg)
+                except json.JSONDecodeError as e:
+                    print("Erreur lors du décodage de la réponse JSON: ", e)
             else:
-                embed_msg = discord.Embed()
-                embed_msg.add_field(name="Message", value=f"Le joueur {summoner_name} est déjà enregistré.", inline=False),
-                embed_msg.colour=discord.Colour.green()
-                await ctx.send(embed=embed_msg)
+                print("Erreur lors de la requête pour récupérer les données du joueur: ", summoner_data.status_code)
         except requests.exceptions.RequestException as e:
             print("Erreur lors de la requête pour récupérer le rang du joueur: ", e)
 
